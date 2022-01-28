49f2cb9f7c220be954620ccb1dcc9710f5a3b932
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-01-28 08:37:39 +0200

    completed exercise.
    Added a workaround for legacy SSL in package.json

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Input from "./Input";
 
-function Form() {
+
+function Form(props) {
   return (
     <form className="form">
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      { 
+        //Using the ternary operator to check if the user is registered and display the corresponding fields and button.
+        
+        !props.userIsRegistered ? <><Input type="password" placeholder="Confirm Password" /><button type="submit">Register</button> </>: <button type="submit">Login</button>
+        
+        }
     </form>
   );
 }
